476c5f2782a4699bbb4929f6015166010b0fbf79
yvonnechen1998 <66394176+yvonnechen1998@users.noreply.github.com>
2020-06-07 22:45:42 -0700

    Add files via upload
    Initial commit

---
+++ quotes.js
@@ -0,0 +1,23 @@
+var quotes = [
+    'I can make your girlfriend scream louder than you ever could just by pressing her follow button',
+    'Swallow your fears. Wipe off your tears. Let go of your darkest years. Chin up. Smile. Walk Away', 
+    'Real girl isn\'t perfect. Perfect girl isn\'t real.', 
+    'Chin Up. Smile. Walk Away.', 
+    'We have a choice. to live or to exist',
+    'Be a lover. Give love. Choose love', 
+    'Treat People With Kindness', 
+    'Please feel free to be whoever yoou want to be in this room ', 
+    'I ask you to please choose love every single day.'
+
+]
+
+// function newQuote() { 
+//     var randomNumber =  Math.floor(Math.random()*(quotes.length));   
+// }
+
+function newQuote() { 
+
+var randomNumber =  Math.floor(Math.random()*(quotes.length));  
+document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber]; 
+
+} 
