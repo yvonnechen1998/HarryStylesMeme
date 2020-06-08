f2b1c6801cbe3b8336bc5db2dbaee844909058cb
yvonnechen1998 <66394176+yvonnechen1998@users.noreply.github.com>
2020-06-08 00:11:24 -0700

    Add files via upload
    added image and favicon

--- quotes.js
+++ quotes.js
@@ -1,12 +1,12 @@
 var quotes = [
-    'I can make your girlfriend scream louder than you ever could just by pressing her follow button',
+    
     'Swallow your fears. Wipe off your tears. Let go of your darkest years. Chin up. Smile. Walk Away', 
     'Real girl isn\'t perfect. Perfect girl isn\'t real.', 
     'Chin Up. Smile. Walk Away.', 
     'We have a choice. to live or to exist',
     'Be a lover. Give love. Choose love', 
     'TREAT PEOPLE WITH KINDNESS', 
-    'Please feel free to be whoever yoou want to be in this room ', 
+    'Please feel free to be whoever you want to be in this room ', 
     'I ask you to please choose love every single day.'
 
 ]
